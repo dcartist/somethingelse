f855715d1487a81c9cdcb7cf2147cf3627c011f3
Paula Bannerman <Pbannerman13@gmail.com>
2022-06-08 22:45:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let player = {
+  name: "Evil guy",
+  number: 218, 
+  height: "5'10",
+  gamesPlayed: [1, 2, 3, 5, 6, 7, 8, 9, 10],
+  play(){
+    console.log("Little kid music")
+  }
+}
+
+
+console.log(player.height)
+console.log(player.gamesPlayed[3])
+console.log(player["height"])
+
+player.play()
